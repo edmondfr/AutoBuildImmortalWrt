27acc41b55420d9342620556fd63a1b42a4f8202
edmondfr <edmondfr@gmail.com>
2025-09-06 17:41:50 +0800

    Update build.sh

--- armsr-armv8/build.sh
+++ armsr-armv8/build.sh
@@ -72,7 +72,7 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
-#PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
+PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 # 若构建openclash 则添加内核
 if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
